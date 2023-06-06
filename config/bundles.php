493fdff15ef8e80880811823e4a6308eb6b025cf
Paul <paulendev@gmail.com>
2023-06-06 11:54:17 +0200

    j'ai re fait sans la merde que j'ai mis avant

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
---
+++ src/Form/ContactType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('sujet', TextType::class, [
+                'attr'=>[
+                    'placeholder'=>'Le sujet de votre message : '
+                ]
+                ])
+            ->add('date')
+            ->add('message', TextareaType::class, [
+                'attr'=>[
+                    'placeholder'=>'Votre message : '
+                ]
+            ])
+            ->add('statut', ChoiceType::class, [
+                'choices'=>[
+                    'Validé'=>'valide',
+                    'Refusé'=>'refuse',
+                    'En Cours'=>'en cours'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
